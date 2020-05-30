8ea0d573ed50997ab06dd3fc9c0ce66434b4616f
Emrullah YILDIRIM <emrullahyildirim@windowslive.com>
2020-05-31 00:42:56 +0300

    Use annatotaion instead of xml conf

--- servlets/servlet/src/main/java/GreetingServlet.java
+++ servlets/servlet/src/main/java/GreetingServlet.java
@@ -1,9 +1,15 @@
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+@WebServlet(
+        name = "GreetingServlet",
+        description = "Example Servlet Using Annotations",
+        urlPatterns = {"/GreetingServlet"}
+)
 public class GreetingServlet extends HttpServlet {
 
     @Override
